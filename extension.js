dd59bbe588373d51ea0def4e11e8ea23738c8d25
ciarmoli-marco <marco.ciarmoli@edu.unito.it>
2019-06-05 15:15:54 +0200

    Added can fresco

--- extension.js
+++ extension.js
@@ -28,12 +28,32 @@ function init() {
     button.connect("clicked", function(){
         Clipboard.set_text(CLIPBOARD_TYPE,  label.get_text()+"000");
     });
+
+    button2 = new St.Button({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: false,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true
+    });
+    label2 = new St.Label({
+        style_class: 'unixtime',
+        text: 'Andrea.Fresco.76adb6@test.com',
+        opacity: 200,
+        x_expand: true
+    });
+    button2.set_child(label2);
+    button2.connect("clicked", function(){
+        Clipboard.set_text(CLIPBOARD_TYPE,  label2.get_text());
+    });
 }
 
 function enable() {
     update_time();
     clock_signal_id = clock.connect('notify::clock', Lang.bind(this, this.update_time));
     Main.panel._centerBox.insert_child_at_index(button, 1);
+    Main.panel._centerBox.insert_child_at_index(button2, 2);
 }
 
 function disable() {
